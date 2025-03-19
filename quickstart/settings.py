55398f2fd2adec16c4c419ca2fbe9eee88a953e5
Sifuniso Simushi <sifunisosimushi@gmail.com>
2025-03-19 03:03:44 +0200

    added postgresql

--- quickstart/settings.py
+++ quickstart/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'quickstart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quickstart_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
